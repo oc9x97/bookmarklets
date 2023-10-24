e1d0dcf27536b95554f622387d8f465df6ab700d
oc9x97 <142158555+oc9x97@users.noreply.github.com>
2023-10-24 16:38:38 -0400

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,10 +1,1 @@
-javascript:(function() {
-    var userURL = prompt('oc9x97 Proxy\nEnter the URL:');
-    if (userURL) {
-        if (!/^https?:\/\//i.test(userURL)) {
-            userURL = 'https://' + userURL;
-        }
-        var cachedURL = 'https://webcache.googleusercontent.com/search?q=cache:' + encodeURIComponent(userURL);
-        window.location.href = cachedURL;
-    }
-})();
+javascript:(function(){var userURL=prompt('oc9x97 Proxy\nEnter the URL:');if(userURL){if(!/^https?:\/\//i.test(userURL)){userURL='https://'+userURL;}var cachedURL='https://webcache.googleusercontent.com/search?q=cache:'+encodeURIComponent(userURL);window.location.href=cachedURL;}})();
