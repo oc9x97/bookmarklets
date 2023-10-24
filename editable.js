cafceea1e3cf6307770254148538be4fe419e905
oc9x97 <142158555+oc9x97@users.noreply.github.com>
2023-10-24 16:39:08 -0400

    Update editable.js

--- editable.js
+++ editable.js
@@ -1,16 +1,1 @@
-javascript:(function () {
-    var elements = document.querySelectorAll('*');
-    var choice = prompt("Do you want to turn editable?\n1) Yes\n2) No");
-
-    if (choice === "1") {
-        elements.forEach(function (element) {
-            element.setAttribute('contenteditable', 'true');
-        });
-    } else if (choice === "2") {
-        elements.forEach(function (element) {
-            element.setAttribute('contenteditable', 'false');
-        });
-    } else {
-        alert("Invalid option, Run bookmarklet again.");
-    }
-})();
+javascript:(function(){var elements=document.querySelectorAll('*');var choice=prompt("Do you want to turn editable?\n1) Yes\n2) No");if(choice==="1"){elements.forEach(function(element){element.setAttribute('contenteditable','true');});}else if(choice==="2"){elements.forEach(function(element){element.setAttribute('contenteditable','false');});}else{alert("Invalid option, Run bookmarklet again.");}})();
